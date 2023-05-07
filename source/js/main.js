96f966b114a590d2aae50ce2bd795bd9b7958d41
constoner <constantin.kalinin@gmail.com>
2023-05-08 00:41:14 +0300

    подключил видео из ютуба

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {onYouTubeIframeAPIReady} from './modules/video/video';
 
 // ---------------------------------
 
@@ -17,7 +17,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
+    setTimeout(() => onYouTubeIframeAPIReady(), 1000);
+    // onYouTubeIframeAPIReady();
     const form = new Form();
     window.form = form;
     form.init();
---
+++ source/js/modules/video/video.js
@@ -0,0 +1,45 @@
+const playButton = document.querySelector('[data-play-button]');
+const videoCover = document.querySelector('[data-video-cover]');
+const videoSRC = document.querySelector('#player').dataset.videoSrc;
+const tag = document.createElement('script');
+const firstScriptTag = document.getElementsByTagName('script')[0];
+
+tag.src = 'https://www.youtube.com/iframe_api';
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+function onYouTubeIframeAPIReady() {
+  // eslint-disable-next-line no-undef
+  player = new YT.Player('player', {
+    videoId: videoSRC,
+    playerVars: {
+      'playsinline': 1,
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange,
+    },
+  });
+}
+
+function onPlayerReady() {
+  playButton.addEventListener('click', () => {
+    videoCover.dataset.videoCoverIsshown = 'false';
+    playButton.dataset.playButtonIsshown = 'false';
+    player.playVideo();
+  });
+}
+
+function onPlayerStateChange(event) {
+  // eslint-disable-next-line no-undef
+  if (event.data === YT.PlayerState.ENDED) {
+    pauseVideo();
+  }
+}
+
+function pauseVideo() {
+  videoCover.dataset.videoCoverIsshown = 'true';
+  playButton.dataset.playButtonIsshown = 'true';
+}
+
+export {onYouTubeIframeAPIReady};
