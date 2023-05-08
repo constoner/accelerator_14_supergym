f590840677c74eeff7e8517a3a2c87f5a6eebc67
constoner <constantin.kalinin@gmail.com>
2023-05-08 21:49:39 +0300

    подключил табы, сделал карточки абонементов для ПК

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,8 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
 import {onYouTubeIframeAPIReady} from './modules/video/video';
+import {initTabs} from './modules/tabs/init-tabs';
+
 
 // ---------------------------------
 
@@ -17,8 +19,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    setTimeout(() => onYouTubeIframeAPIReady(), 1000);
-    // onYouTubeIframeAPIReady();
+    setTimeout(() => onYouTubeIframeAPIReady(), 1500);
+    initTabs();
     const form = new Form();
     window.form = form;
     form.init();
